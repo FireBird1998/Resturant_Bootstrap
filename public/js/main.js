eda689c7d7b3ca1efa0bc3dc5e7a50fa242e9279
Ankit Das <www.das5471@gmail.com>
2023-08-08 03:59:57 +0530

    nab bar style and js

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,12 @@
 //Active nav Bar
 const nav = document.querySelector(".navigation-wrap");
-
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+    }
+}
 
 
 
