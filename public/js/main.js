e918a746ea9b37fa597d50a8c741933ebc671ba0
Ankit Das <www.das5471@gmail.com>
2023-08-08 03:54:57 +0530

    counter section complete and js started

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+//Active nav Bar
+const nav = document.querySelector(".navigation-wrap");
+
+
+
+
+
+
+//Counter Actions
+
+document.addEventListener("DOMContentLoaded", () => {
+    function Counter(id, start, end, duration) {
+        var obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration/ range)),
+        time = setInterval(() => {
+            current += increment;
+            obj.textContent = current;
+            if(current === end){
+                clearInterval(time);
+            }
+        }, step);
+    }
+    Counter("count1", 600, 1287, 3000);
+    Counter("count2", 500, 1398, 2500);
+    Counter("count3", 500, 2100, 1000);
+    Counter("count4", 2000, 2400, 2000);
+
+});
